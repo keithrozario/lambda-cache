e1ee043704bf66b6cc94b71b72e4a8eb9bc65e04
keithrozario <keithjosephrozario@gmail.com>
2020-03-29 15:38:56 +0800

    black reformatter

--- aws_lambda_cache/__init__.py
+++ aws_lambda_cache/__init__.py
@@ -1,5 +1,4 @@
-__version__ = '0.2.5'
-__all__ = ['ssm_cache']
+__version__ = "0.2.5"
+__all__ = ["ssm_cache"]
 
 from .ssm import ssm_cache
-
--- aws_lambda_cache/ssm.py
+++ aws_lambda_cache/ssm.py
@@ -2,6 +2,7 @@ import functools
 import time
 import boto3
 
+
 def ssm_cache(parameter, ttl_seconds=60, var_name=False):
     """
     Get_parameter in SSM parameter store, and injects it into the event object of the lambda function
@@ -20,7 +21,6 @@ def ssm_cache(parameter, ttl_seconds=60, var_name=False):
     """
 
     def decorator(func):
-
         @functools.wraps(func)
         def inner_function(event, context):
 
@@ -28,13 +28,18 @@ def ssm_cache(parameter, ttl_seconds=60, var_name=False):
             parameter_age = get_parameter_age(parameter_name)
 
             if parameter_age is None:
-                event[parameter_name] = get_parameter_from_ssm(parameter, parameter_name)
+                event[parameter_name] = get_parameter_from_ssm(
+                    parameter, parameter_name
+                )
             elif parameter_age < ttl_seconds:
                 event[parameter_name] = get_parameter_from_cache(parameter_name)
             else:
-                event[parameter_name] = get_parameter_from_ssm(parameter, parameter_name)
+                event[parameter_name] = get_parameter_from_ssm(
+                    parameter, parameter_name
+                )
 
             return func(event, context)
+
         return inner_function
 
     return decorator
@@ -51,13 +56,14 @@ def get_parameter_name(parameter, var_name):
     Returns:
         parameter_name   : Name of parameter stored in global_aws_lambda_cache map
     """
-    
+
     if var_name:
         parameter_name = var_name
     else:
-        parameter_name = parameter.replace('/', '_')
-    
+        parameter_name = parameter.replace("/", "_")
+
     return parameter_name
+
 
 def get_parameter_age(parameter_name):
     """
@@ -70,16 +76,24 @@ def get_parameter_age(parameter_name):
     global global_aws_lambda_cache
 
     try:
-        get_param_timestamp = global_aws_lambda_cache[parameter_name]['get_param_timestamp']
+        get_param_timestamp = global_aws_lambda_cache[parameter_name][
+            "get_param_timestamp"
+        ]
         parameter_age = int(time.time() - get_param_timestamp)
     except NameError:  # create global_aws_lambda_cache
-        global_aws_lambda_cache = {parameter_name: {'value': None, 'get_param_timestamp': None}}
+        global_aws_lambda_cache = {
+            parameter_name: {"value": None, "get_param_timestamp": None}
+        }
         parameter_age = None
-    except KeyError: # parameter doesn't exist in global_aws_lambda_cache
-        global_aws_lambda_cache[parameter_name] = {'value': None, 'get_param_timestamp': None}
+    except KeyError:  # parameter doesn't exist in global_aws_lambda_cache
+        global_aws_lambda_cache[parameter_name] = {
+            "value": None,
+            "get_param_timestamp": None,
+        }
         parameter_age = None
 
     return parameter_age
+
 
 def get_parameter_from_ssm(parameter, parameter_name):
     """
@@ -91,18 +105,18 @@ def get_parameter_from_ssm(parameter, parameter_name):
         parameter_value (string): Value of parameter in Parameter Store
     """
 
-    ssm_client = boto3.client('ssm')
-    response = ssm_client.get_parameter(
-        Name=parameter,
-        WithDecryption=True
-    )
-    parameter_value = response['Parameter']['Value']
+    ssm_client = boto3.client("ssm")
+    response = ssm_client.get_parameter(Name=parameter, WithDecryption=True)
+    parameter_value = response["Parameter"]["Value"]
 
     global global_aws_lambda_cache
-    global_aws_lambda_cache[parameter_name] = {'value': parameter_value,
-                                               'get_param_timestamp': time.time()}
+    global_aws_lambda_cache[parameter_name] = {
+        "value": parameter_value,
+        "get_param_timestamp": time.time(),
+    }
 
     return parameter_value
+
 
 def get_parameter_from_cache(parameter_name):
     """
@@ -115,5 +129,5 @@ def get_parameter_from_cache(parameter_name):
     """
 
     global global_aws_lambda_cache
-    parameter_value = global_aws_lambda_cache.get(parameter_name).get('value')
+    parameter_value = global_aws_lambda_cache.get(parameter_name).get("value")
     return parameter_value
