fa22eb6cf581a5926f93b2523ef3f346db8b277e
keithrozario <keithjosephrozario@gmail.com>
2020-04-13 09:13:09 +0800

    fixed terraform for version 0.12

--- tests/test_env/s3.tf
+++ tests/test_env/s3.tf
@@ -1,19 +1,20 @@
 # Provider Block
 provider "aws" {
-  version    = "~> 2.7"
+  version = "~> 2.7"
   # region -> Taken from env vars or defaulted from profile
 }
 
 resource "aws_s3_bucket" "test_s3bucket" {
   bucket_prefix = "lambda-cache"
-  acl    = "private"
+  acl           = "private"
   force_destroy = true
 }
 
 resource "aws_ssm_parameter" "test_s3bucket_name" {
-  type  = "String"
+  type        = "String"
   description = "Name of s3 bucket to hold layer artifacts"
-  name = "/lambda-cache/s3/bucket_name"
-  value = "${aws_s3_bucket.test_s3bucket.bucket}"
-  overwrite = true
+  name        = "/lambda-cache/s3/bucket_name"
+  value       = aws_s3_bucket.test_s3bucket.bucket
+  overwrite   = true
 }
+
